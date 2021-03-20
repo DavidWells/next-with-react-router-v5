cd93d2386396de896cc1f6a8818d5c7e5ca3313b
DavidWells <>
2021-03-19 19:22:50 -0700

    deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  target: 'serverless',
+  poweredByHeader: false,
+  env: {
+    SITE_NAME: 'foo',
+  },
   async rewrites() {
     return [
       // Rewrite everything else to use `pages/index`
